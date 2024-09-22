11f6e4c036297159c249ec1cc76b77d7329cc384
Chandan Kumar <chandanagni91@gmail.com>
2024-09-22 21:20:50 +0530

    DSA practice questions

---
+++ src/array/MergeSortedArray.java
@@ -0,0 +1,33 @@
+package array;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+
+    public static int[] merge(int[] nums1, int m, int[] nums2, int n){
+
+        int r1 = m - 1;
+        int r2 = n - 1;
+        for (int p = m + n - 1; p >= 0; p--){
+            if (r2 < 0) break;
+            if (r1 >= 0 && nums1[r1] > nums2[r2]){
+                nums1[p] = nums1[r1];
+                r1--;
+            } else {
+                nums1[p] = nums2[r2];
+                r2--;
+            }
+        }
+
+
+        return nums1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1,2,3,0,0,0};
+        int[] nums2 = new int[]{2,5,6};
+
+        System.out.println(Arrays.toString(merge(nums1, 3, nums2, 3)));
+
+    }
+}
---
+++ src/array/RotateArray.java
@@ -0,0 +1,51 @@
+package array;
+
+import java.util.Arrays;
+
+public class RotateArray {
+
+    public static void rotate(int[] nums, int k){
+        k = k % nums.length;
+        int[] rotated = Arrays.copyOfRange(nums, nums.length-k, nums.length);
+        int left = nums.length - k - 1;
+        int right = nums.length - 1;
+        while (left >= 0){
+            nums[right] = nums[left];
+            right--;
+            left--;
+        }
+        k--;
+        while (k >= 0){
+            nums[right] = rotated[k];
+            right--;
+            k--;
+        }
+    }
+
+    public static void rotateConstantSpace(int[] nums, int k){
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length ; start++){
+            int current = start;
+            int prev = nums[current];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                current = next;
+                prev = temp;
+                count++;
+
+            }while (start != current);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5,6,7};
+        rotate(nums, 3);
+        System.out.println(Arrays.toString(nums));
+        nums = new int[]{1,2,3,4,5,6,7};
+        rotateConstantSpace(nums, 3);
+        System.out.println(Arrays.toString(nums));
+    }
+}
